285d946ce901b96c2f669f970768b1a89bcf31b5
DulceT7 <dataguite@hotmail.com>
2025-03-28 12:30:34 -0600

    Add files via upload

--- Examen.java
+++ Examen.java
@@ -1,15 +1,18 @@
+import java.util.Scanner;
 public class Examen{
     public static void main(String [] args){
 
 
         Scanner scanner = new Scanner(System.in);
-        
-        int opcion;
+
 
         System.out.println("[1] la suma de un número y su triple");
         System.out.println("[2] el doble de un numero menos 5");
         System.out.println("[3]La mitad de un número más su cuadrado");
         System.out.println("[4]La suma de un número su doble y el triple");
+        System.out.println("[?] : ");
+
+        int opcion = scanner.nextInt();
 
         switch (opcion){
             case 1:
@@ -17,8 +20,7 @@ public class Examen{
             System.out.println("<------------------>");
             System.out.println("Primer valor de x");
             int n1 = scanner.nextInt();
-            double resultaado = n1+n1*3;
-
+            double resultaado = 4*n1;
             System.out.println("Resultado" + resultaado);
 
             break;
@@ -37,7 +39,7 @@ public class Examen{
             System.out.println("<------------->");
             System.out.println("Tercer valor de x");
             int n3 = scanner.nextInt();
-            double resultado3 = n3/2+n3*2;
+            double resultado3 = (2.0/n3) + (n3*n3);
             System.out.println("resultado " + resultado3);
 
             break;
@@ -46,19 +48,16 @@ public class Examen{
             System.out.println("<-------------->");
             System.out.println("Cuarto valor de x");
             int n4 = scanner.nextInt();
-             double resultado4 = n4+n4*2+n4*3;
+             double resultado4 = n4 + (n4*2) + (n4*3);
              System.out.println("resultado " + resultado4);
-
 
             break;
 
-
-
-
-
-
-
+            default:
+            System.out.println("Opción no valida");
 
         }
+
+        System.out.println("Saliendo del programa");
     }
 }
